60991b600eb02fecd5e9978e80d36d406b42341a
Nafiz Bayrak <nafizbay@hotmail.com>
2020-04-21 23:07:22 +0300

    Message (commit on 'master')

--- bir/src/app/sevices/category.service.spec.ts
+++ bir/src/app/sevices/category.service.spec.ts
@@ -1,9 +1,17 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 
 import { CategoryService } from './category.service';
 
 describe('CategoryService', () => {
   let service: CategoryService;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [CategoryService]
+  }));
+
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
